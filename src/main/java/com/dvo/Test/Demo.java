a79844b41d7295805a4c4c52230aac67dc146908
'zhangjiangfeng' <'jiangfeng.zhang@diveo.com.cn'>
2019-07-16 13:52:48 +0800

    test

---
+++ src/main/java/com/dvo/Test/Demo.java
@@ -0,0 +1,20 @@
+/**
+* @version: 
+* @Description:
+* @author: Zhang Jiangfeng
+* @date: 2019年7月16日
+*/ 
+package com.dvo.Test;
+
+/** 
+* @version:
+* @Description:
+* @author: Zhang Jiangfeng
+* @date: 2019年7月16日 
+*/
+public class Demo {
+
+	public String str;
+	public Integer i;
+	
+}
